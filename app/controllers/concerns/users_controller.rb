7a0e50e36277386cd08ced24a3a40094a1a5849e
xxxx <makomakomako19960619@i.softbank.jp>
2020-01-08 00:55:18 +0900

    インクリメンタルサーチ実装

--- app/controllers/concerns/users_controller.rb
+++ app/controllers/concerns/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
 
-  def edit
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
-
+  
   def update
     if current_user.update(user_params)
       redirect_to root_path
@@ -16,4 +22,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email)
   end
-end
+end   
