e89eaefa17b4757758172e180218a6be6cf21146
xxxx <makomakomako19960619@i.softbank.jp>
2020-01-14 14:51:48 +0900

    完了

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -8,7 +8,7 @@ $(function(){
              ${message.user_name}
            </div>
            <div class="upper-message__date">
-             ${message.date}
+             ${message.created_at}
            </div>
          </div>
          <div class="lower-message">
@@ -27,7 +27,7 @@ $(function(){
              ${message.user_name}
            </div>
            <div class="upper-message__date">
-             ${message.date}
+             ${message.created_at}
            </div>
          </div>
          <div class="lower-message">
@@ -75,12 +75,14 @@ var reloadMessages = function() {
     data: {id: last_message_id}
   })
   .done(function(messages) {
+
     if (messages.length !== 0) {
       //追加するHTMLの入れ物を作る
       var insertHTML = '';
       //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
       $.each(messages, function(i, message) {
         insertHTML += buildHTML(message)
+  
       });
       //メッセージが入ったHTMLに、入れ物ごと追加
       $('.messages').append(insertHTML);
@@ -93,8 +95,7 @@ var reloadMessages = function() {
     alert('自動更新に失敗しました');
   });
 };
-    setInterval(reloadMessages, 7000);
-    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-      setInterval(reloadMessages, 7000);
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
     } 
 });
